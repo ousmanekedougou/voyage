56da421e88bf86c8baf40d1c31e24adfe6f3cefd
ousmanekedougou <yabaye075@gmail.com>
2022-01-19 12:12:50 +0000

    historical

--- resources/views/user/setting/index.blade.php
+++ resources/views/user/setting/index.blade.php
@@ -9,7 +9,7 @@
             <div class="row align-items-center" style="margin-top: -70px;">
                 <div class="col-lg-8">
                     <div class="text-white-50">
-                        <h1 class="text-white font-weight-semibold mb-3 hero-title">Yombalal Toucki Thi Biir Rewmi</h1>
+                        <h1 class="text-white font-weight-semibold mb-3 hero-title">Yombalal Toucki Thi Biir Réwmi</h1>
                         <p class="font-size-20">
                             Le secteur du tourisme a fort à faire dans une conjoncture économique pleine de défis. Pour vous démarquer, en tant qu’agence de voyages.
                         </p>
