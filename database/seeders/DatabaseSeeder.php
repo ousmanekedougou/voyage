11b5713a616990c81de856bd8899cc5b6a29ac95
ousmanekedougou <yabaye075@gmail.com>
2022-02-04 17:16:23 +0000

    reglage du seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,14 +18,27 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         User::create([
             'name' => 'Ousmane Diallo',
-            'email' => "email@gmail.com",
+            'email' => "toucki@gmail.com",
             'phone' => '221770000000',
             'adress' => 'Kedougou',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
             'status' => 1,
             'is_active' => 1,
+            'is_admin' => 0,
+            'slug' => str_replace('/','',Hash::make(Str::random(20).'admin/toucki@gmail.com')),
+            'confirmation_token' => null
+        ]);
+
+        User::create([
+            'name' => 'Oumar Barry',
+            'email' => "barry@gmail.com",
+            'phone' => '221780000000',
+            'adress' => 'Kedougou',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+            'status' => 1,
+            'is_active' => 1,
             'is_admin' => 1,
-            'slug' => str_replace('/','',Hash::make(Str::random(20).'admin/email@gmail.com')),
+            'slug' => str_replace('/','',Hash::make(Str::random(20).'admin/barry@gmail.com')),
             'confirmation_token' => null
         ]);
     }
