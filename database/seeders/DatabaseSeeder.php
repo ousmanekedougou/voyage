0e9bf37c19d3ab7f0a9dd61538bf8d6b45eb8188
ousmanekedougou <yabaye075@gmail.com>
2022-02-13 00:33:12 +0000

    faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use \Faker\Factory as Faker;
+use Faker\Factory as Faker;
 class DatabaseSeeder extends Seeder
 {
     /**
